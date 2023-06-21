e31cecaa79bae84e3103dc10fe7874ff1902d586
jhree333 <136793299+jhree333@users.noreply.github.com>
2023-06-21 12:59:30 +0900

    feat: 블로그 목록 페이지 생성

---
+++ src/components/posts/PostCard.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import { IPost } from "../../commons/types/types";
+
+export default function PostCard({ post }: { post: IPost }) {
+  return (
+    <Link href={`/post/${post.slug}`}>
+      <article>
+        <Image
+          className="border rounded-lg"
+          src={`/${post.frontMatter.image}`}
+          width={1200}
+          height={700}
+          alt={post.frontMatter.title}
+        />
+        <div className="px-2 py-4">
+          <h2 className="font-bold text-lg">{post.frontMatter.title}</h2>
+          <time>{post.frontMatter.date}</time>
+        </div>
+      </article>
+    </Link>
+  );
+}
